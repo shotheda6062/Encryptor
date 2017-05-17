3d005edc68de2be19a8260dd036c90baf49ff1e7
Clarke Yeh <shothead6062@gmail.com>
2017-05-17 14:40:35 +0800

    C# Studio
    C# 練習

---
+++ Encryptor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryptor
+{
+    class Program
+    {
+        class Encrypt
+        {
+            public string cArray;
+
+            public void setArray()
+            {
+                int a = 3;
+                int b = 5;
+                int x, y, m;
+                char c = 'a';
+                int i;
+                System.Text.StringBuilder s = new System.Text.StringBuilder();
+                for (i = 0; i < 26; i++)
+                {
+                    x = c;
+                    y = x * a + b;
+                    m = y % 26;
+                    s.Append((char)(m + 97));
+                    c++;
+                }
+
+                this.cArray = s.ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Encrypt e = new Encrypt();
+            e.setArray();
+            System.Console.WriteLine("e: " + e.cArray);
+            Console.Read();
+
+        }
+    }
+}
